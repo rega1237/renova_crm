2b08b1cf54b499f8afb56c09984d3cc9c6457655
Rafael Guzman <rega1237@gmail.com>
2025-11-17 13:48:00 -0400

    ci: update CI workflow and test configuration
    - Update test database configuration to use environment variables
    - Configure headless Chrome for system tests
    - Simplify Chrome installation in CI workflow
    - Add environment variables for test database and parallel workers

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,8 +1,18 @@
 require "test_helper"
+require "capybara/rails"
+
+Capybara.register_driver :chrome_headless_ci do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  options.add_argument("--headless=new")
+  options.add_argument("--disable-gpu")
+  options.add_argument("--no-sandbox")
+  options.add_argument("--disable-dev-shm-usage")
+  options.add_argument("--window-size=1400,1400")
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
+end
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  # Usar Selenium con Chrome para soportar JavaScript en pruebas de sistema
-  driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ]
+  driven_by :chrome_headless_ci
 
   setup do
     login_as_admin
